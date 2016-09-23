e35b319f52ef01ed485f43a9f16f31bb655f48aa
zhangdongjava <787395919@qq.com>
2016-09-23 15:48:06 +0800

    消息改对象

--- draw/src/main/java/com/zzz/draw/tcp/TcpClientHandler.java
+++ draw/src/main/java/com/zzz/draw/tcp/TcpClientHandler.java
@@ -14,48 +14,48 @@ import java.util.concurrent.TimeUnit;
 
 public class TcpClientHandler extends SimpleChannelInboundHandler<Message> {
 
-	private TcpClient tcpClient;
+    private TcpClient tcpClient;
 
-	public TcpClientHandler(TcpClient tcpClient) {
-		this.tcpClient = tcpClient;
-	}
+    public TcpClientHandler(TcpClient tcpClient) {
+        this.tcpClient = tcpClient;
+    }
 
-	@Override
-	protected void channelRead0(ChannelHandlerContext ctx, Message msg)
-			throws Exception {
-		if(MessageCode.STRING == msg.getType())
-		System.out.println("client接收到服务器返回的消息:"+ BeanDecoder.stringParseForm(msg.getByteBuf()));
-		
-	}
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, Message msg)
+            throws Exception {
+        if (MessageCode.STRING == msg.getType())
+            System.out.println("client接收到服务器返回的消息:" + BeanDecoder.stringParseForm(msg.getByteBuf()));
+
+    }
 
 
-	@Override
-	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
-		if(cause instanceof IOException){
-			try {
-				System.out.println("IOException");
-				ctx.channel().disconnect();
-				reConnect(ctx);
-			}catch (Exception e){
-				reConnect(ctx);
-				System.out.println(e.toString());
-			}
-		}
-	}
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        if (cause instanceof IOException) {
+            try {
+                System.out.println("IOException");
+                ctx.channel().disconnect();
+                reConnect(ctx);
+            } catch (Exception e) {
+                reConnect(ctx);
+                System.out.println(e.toString());
+            }
+        }
+    }
 
-	private void reConnect(ChannelHandlerContext ctx){
-		try {
-			Channel res = tcpClient.getChannel(tcpClient.HOST, tcpClient.PORT);
-			while(res ==null){
-				TimeUnit.SECONDS.sleep(1);
-				res = tcpClient.getChannel(tcpClient.HOST, tcpClient.PORT);
-				System.out.println("重新连接。。。!");
-			}
-			System.out.println("连接成功!。。。!");
-		}catch (Exception e){
-			reConnect(ctx);
-			System.out.println(e.toString());
-		}
+    private void reConnect(ChannelHandlerContext ctx) {
+        try {
+            Channel res = tcpClient.getChannel(tcpClient.HOST, tcpClient.PORT);
+            while (res == null) {
+                TimeUnit.SECONDS.sleep(1);
+                res = tcpClient.getChannel(tcpClient.HOST, tcpClient.PORT);
+                System.out.println("重新连接。。。!");
+            }
+            System.out.println("连接成功!。。。!");
+        } catch (Exception e) {
+            reConnect(ctx);
+            System.out.println(e.toString());
+        }
 
-	}
+    }
 }
